cb0a8916f010d25dfdb8bc81464f7c0670fe2b3f
vivianknee <vivianni452@gmail.com>
2025-08-26 21:40:35 -0700

    frontend for signup and login

---
+++ frontend/financetracker/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
+
+const Root = () => {
+  //check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  //redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+};
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path= "/" element={<Root />} />
+          <Route path= "/login" exact element={<Login />} />
+          <Route path= "/signup" exact element={<SignUp />} />
+          <Route path= "/dashboard" element={<Home />} />
+          <Route path= "/income" element={<Income />} />
+          <Route path= "/expense" element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
