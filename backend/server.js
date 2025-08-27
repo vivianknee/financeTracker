499eed8da015644bf92da6221e58ac570991f25c
vivianknee <vivianni452@gmail.com>
2025-08-27 13:54:45 -0700

    port change

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,5 @@ app.use('*', (req, res) => {
     res.status(404).json({ message: 'Route not found' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
