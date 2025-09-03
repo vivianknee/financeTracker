d9792c71ecedc2730ccd7f1a6cac31d2dc32c4f5
vivianknee <vivianni452@gmail.com>
2025-09-03 12:22:45 -0400

    temp

--- frontend/financetracker/src/utils/apiPaths.js
+++ frontend/financetracker/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
-export const BASE_URL = import.meta.env.VITE_API_URL || "https://finance-tracker-backend-coral.vercel.app";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+//export const BASE_URL = import.meta.env.VITE_API_URL || "https://finance-tracker-backend-coral.vercel.app";
 
 //utils/apiPaths.js
 export const API_PATHS = {
